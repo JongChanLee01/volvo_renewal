ec84b39ec770bd1c01df712c60dae0806b41e4a7
JongChanLee <76044533+JongChanLee01@users.noreply.github.com>
2023-08-03 22:28:34 +0900

    volvo renewal

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import react, {useState} from 'react';
+import React, {useState} from 'react';
 import Mainslider from './component/Mainslider';
 import Model from './component/Model';
 import Content from './component/Content';
